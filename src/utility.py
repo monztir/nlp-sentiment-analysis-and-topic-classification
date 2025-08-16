bb34bed22a48da1767ec5d13211b9ff96d0164d1
Savo Savić <224417723+monztir@users.noreply.github.com>
2025-08-16 09:40:09 +0200

    feat: add 20Newsgroups dataset exploration with newsgroup distribution analysis
    Note:
    Additional changes:
     - Removed wrong 20Newsgroups dataset (subset of original) and added original 20Newsgroups
     - Updated 'utility.py', to be precise 'load_data_20newsgroups' because of different dataset structure

--- src/utility.py
+++ src/utility.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def load_data_imdb(filepath_imdb):
@@ -8,6 +9,25 @@ def load_data_imdb(filepath_imdb):
 
 def load_data_20newsgroups(filepath_20newsgroups):
     
-    data_20newsgroups = pd.read_csv(filepath_20newsgroups)
+    rows = []
+
+    for newsgroup in os.listdir(filepath_20newsgroups):
+
+        newsgroup_path = os.path.join(filepath_20newsgroups, newsgroup)
+
+        print(f"{newsgroup}")
+        print(f"{newsgroup_path}")
+
+        if os.path.isdir(newsgroup_path):
+
+            for file_name in os.listdir(newsgroup_path):
+
+                document_id = os.path.splitext(file_name)[0]
+                rows.append({'newsgroup': newsgroup, 'document_id': document_id})
+                    
+    data_20newsgroups = pd.DataFrame(rows)
+    data_20newsgroups.to_csv('../data/raw/20_newsgroup/list.csv', index=False)
+
+    print(data_20newsgroups.head())
 
     return data_20newsgroups
